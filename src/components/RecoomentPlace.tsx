b5d07c87bd64c2215f57c5a7c18734f6fb5b93c6
Lee hwan seuk <sioo1014@naver.com>
2025-12-05 01:54:35 +0900

    feat: 관광지 업로드 페이지 ui 수정

--- src/components/RecoomentPlace.tsx
+++ src/components/RecoomentPlace.tsx
@@ -1,14 +1,14 @@
 const RecommendPlace = () =>{
   return(
     <div className="flex gap-4 ">
-        <div className="flex-1 w-[104px] h-[104px] bg-[#c4c4c4]"></div>
+        <div className="w-[104px] aspect-[1/1] bg-[#c4c4c4] rounded-[12px]"></div>
         <div className="flex-1 flex-col">
-            <p className="mt-1">성산 일출봉</p>
-            <div className="flex gap-2">
-              <p>제주 서귀포시 성산읍</p>
-              <button>복사사</button>       
+            <p className="mt-1 font-semibold text-[18px] mb-1.5">성산 일출봉</p>
+            <div className="flex gap-2 mb-2 items-center">
+              <p className="w-[169px] text-[#767676] text-[14px]">제주 서귀포시 성산읍</p>
+              <button className="bg-[#e1e1e1] w-[38px] h-[24px] rounded-[8px] text-[12px] font-medium">복사</button>       
             </div>
-            <button>사진 업로드</button>
+            <button className="w-full rounded-[12px] h-10 bg-white">사진 업로드</button>
 
         </div>
     </div>
--- src/pages/Selection/SelectCompletion.tsx
+++ src/pages/Selection/SelectCompletion.tsx
@@ -13,23 +13,28 @@ const SelectCompletion = () => {
   });
 
   // 임시 데이터 - 백엔드에서 받을 주소 목록 (테스트용)
+  const testPlaces: PlaceData[] = [
+    {
+      address: '제주특별자치도 서귀포시 성산읍 성산리',
+      placeName: '성산일출봉',
+    },
+    {
+      address: '제주특별자치도 서귀포시 천지동',
+      placeName: '천지연폭포',
+    },
+    {
+      address: '제주특별자치도 제주시 한림읍 협재리',
+      placeName: '협재해수욕장',
+    },
+    {
+      address: '제주특별자치도 제주시 애월읍',
+      placeName: '애월 카페거리',
+    },
+  ];
+
   useEffect(() => {
     // 지도 로드 후 약간의 딜레이를 주고 마커 추가
     const timer = setTimeout(() => {
-      const testPlaces: PlaceData[] = [
-        {
-          address: '제주특별자치도 서귀포시 성산읍 성산리',
-          placeName: '성산일출봉',
-        },
-        {
-          address: '제주특별자치도 서귀포시 천지동',
-          placeName: '천지연폭포',
-        },
-        {
-          address: '제주특별자치도 제주시 한림읍 협재리',
-          placeName: '협재해수욕장',
-        },
-      ];
 
       // 각 장소마다 마커 추가
       testPlaces.forEach((place, index) => {
@@ -43,7 +48,7 @@ const SelectCompletion = () => {
   }, [addMarkerByAddress]);
 
   return (
-    <div className='flex flex-col'>
+    <div className='flex flex-col h-[100dvh] max-h-[100dvh]'>
       <div className='flex gap-2 pt-8 bg-[#262626] pb-10 -mx-5 px-5'>
         <div className='flex flex-col gap-[14px]'>
           <p className='text-white font-bold leading-[1.5]  text-[20px]'>
@@ -54,16 +59,15 @@ const SelectCompletion = () => {
         </div>
         <div className='w-[103px] h-[110px] bg-gray-900'></div>
       </div>
-      <div className="rounded-t-[20px] rounded-b-none -mx-5 px-5 pt-6">
+      <div className="flex-1 flex flex-col rounded-t-[20px] rounded-b-none -mx-5 px-5 pt-6 pb-6 bg-[#F7F7F8] min-h-0">
         <p className="font-bold text-[#000000] text-[18px]  mb-[14px]">나만의 관광지를 지도로 확인하세요</p>
         <div ref={mapContainer} className="w-full aspect-[67/40] rounded-[12px] mb-10"></div>
-        <div>
+        <div className='flex-1 flex flex-col min-h-0'>
           <p className='font-bold text-[18px] mb-[14px]'>관광지를 방문하고 사진을 업로드해보세요</p>
-          <div className='flex flex-col gap-4'>
-            <RecommendPlace />
-            <RecommendPlace />
-            <RecommendPlace />
-            <RecommendPlace />
+          <div className='flex-1 flex flex-col gap-4 overflow-y-auto [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]'>
+            {testPlaces.map((place, index) => (
+              <RecommendPlace key={index} />
+            ))}
           </div>
         </div>
       </div>
