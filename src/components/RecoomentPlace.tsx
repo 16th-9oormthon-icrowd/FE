e45c48dd369179b567c856bcc1f24cf61d7ef3a4
Lee hwan seuk <sioo1014@naver.com>
2025-12-05 05:04:04 +0900

    fix: 사진 업로드 api 수정

--- src/components/RecoomentPlace.tsx
+++ src/components/RecoomentPlace.tsx
@@ -50,8 +50,8 @@ const RecommendPlace = ({
       </div>
       <div className='flex-1 flex-col'>
         <p className='mt-1 font-semibold text-[18px] mb-1.5'>{placeName}</p>
-        <div className='flex gap-2 mb-2 items-center'>
-          <p className='w-[169px] text-[#767676] text-[14px]'>{address}</p>
+        <div className='flex gap-2 mb-2 '>
+          <p className='w-[169px] text-[#767676] text-[14px] '>{address}</p>
           <button
             onClick={handleCopyAddress}
             className='bg-[#e1e1e1] w-[38px] h-[24px] rounded-[8px] text-[12px] font-medium'
