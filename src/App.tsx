f49f4ae4208090a8ee8fa05ca7acdc30c76c580e
Lee hwan seuk <sioo1014@naver.com>
2025-12-04 21:44:16 +0900

    feat: 온보딩 완료 페이지 ui 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from './layout/Layout';
 import Home from './pages/Home/Home';
+import Completion from './pages/onboarding/Completion';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route element={<Layout />}>
             <Route index element={<Home />} />
+            <Route path='/onboarding/completion' element={<Completion />} />
           </Route>
         </Routes>
       </BrowserRouter>
